bea320fec3e3e00df20d0f903998d0687c19d62e
Aviva Solovey <asolovey@terpmail.umd.edu>
2025-04-07 16:38:04 -0400

    Aviva edited the file

--- exercise-collaborative-programming/file.py
+++ exercise-collaborative-programming/file.py
@@ -2,6 +2,8 @@ print("Hello, Git!")
 # Practicing git add, git commit, and git push
 print("This is a change I made!: Nathan C")
 
+print("This is a change that Aviva S. made!")
+
 # Checks if a number is even (This function was used to practice merge conflict)
 def is_even(number):
     if number % 2 == 0:
