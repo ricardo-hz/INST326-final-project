da600c07a7b8319db59b5c7f6c7e6e867a3b5e32
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-04-07 10:45:07 -0400

    Add is_even() method

--- exercise-collaborative-programming/file.py
+++ exercise-collaborative-programming/file.py
@@ -1,2 +1,17 @@
 print("Hello, Git!")
 print("This is a change I made!: Nathan C")
+
+# Checks if a number is even
+def is_even(number):
+    if number == 1:
+        return False
+    elif number == 2:
+        return True
+    elif number == 3:
+        return False
+    elif number == 4:
+        return True
+    elif number == 5:
+        return False
+    elif number == 6:
+        return True
