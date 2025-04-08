4c2cedf3d21aa335bf1c490a19c4e65a3de4164a
phoenix.pbt1724@gmail.com <cracker@Tamaras-MacBook-Air.local>
2025-04-08 14:03:13 -0400

    made a far far far superior function to check if a number is even

--- exercise-collaborative-programming/file.py
+++ exercise-collaborative-programming/file.py
@@ -10,3 +10,15 @@ def is_even(number):
         return True
     else:
         return False
+    
+def is_even_but_better(number):
+    # from phoenix. this is totally a better way of implementing this.
+    if number < 0:
+        # all negative numbers are OBVIOUSLY even.
+        return True
+    elif number == 0:
+        return True
+    elif number == 1:
+        return False
+    else:
+        return is_even_but_better(number - 2)
