0bbeb996fa12cc2f9deb320e412d36a0e1cdf576
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-04-16 19:55:20 -0400

    Begin writing Character methods, add equipment.py

--- early-algorithms/character.py
+++ early-algorithms/character.py
@@ -1,5 +1,7 @@
+import equipment
+
 class Character():
-    """Represents a character
+    """Represents a character.
     
     This class is only being used for demonstration purposes and early algorithm
     testing.
@@ -10,3 +12,40 @@ class Character():
     Methods:
         something (self, arg, arg) : 
     """
+    
+    def __init__(self, hp, weapon = None, armor = None, abilities = None):
+        """Initializes a new character object.
+        
+        Args:
+            hp (float) : The character's starting health points.
+            weapon (Weapon) : The character's starting weapon. None if omitted.
+            armor (Armor) : The character's starting armor. None if omited.
+            abilities (list of Ability) : The character's starting abilities.
+                None if ommitted. 
+        
+        TODO:
+            - Should weapon be a list of weapon objects? or will characters 
+            have one weapon only?
+        """
+        self.hp = hp
+        self.weapon = weapon
+        self.armor = armor
+        self.abilities = abilities
+        
+    def attack(self, other_character):
+        pass
+    
+    def add_weapon(self, weapon):
+        pass
+    
+    def remove_weapon(self, weapon):
+        pass
+    
+    def add_ability(self, ability):
+        if ability not in self.abilities:
+            self.abilities += ability 
+    
+    # Methods needed for group members:
+        # Some sort of method needed for abilities for Ricardo
+        # Attack method for Nathan
+        # Aviva is doing something with equipment (weapon and armor)
---
+++ early-algorithms/equipment.py
@@ -0,0 +1,11 @@
+# Idk if we'll actually need all or any of these classes, I'm just putting them 
+# here as a foundation of sorts
+
+class Weapon():
+    pass
+
+class Armor():
+    pass
+
+class Ability():
+    pass
