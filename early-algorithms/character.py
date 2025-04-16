2650405e7141ddd5a72a9a189b6e9d7a73863c8e
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-04-16 19:39:05 -0400

    Begin writing character class

---
+++ early-algorithms/character.py
@@ -0,0 +1,12 @@
+class Character():
+    """Represents a character
+    
+    This class is only being used for demonstration purposes and early algorithm
+    testing.
+    
+    Attributes:
+        something (type) : 
+        
+    Methods:
+        something (self, arg, arg) : 
+    """
