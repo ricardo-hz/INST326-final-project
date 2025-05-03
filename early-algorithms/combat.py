9c74193f0dc9c826da7b1bd9083d346ff1abc721
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-05-03 16:54:32 -0400

    Begin working on party info function, remove character subclasses

--- early-algorithms/combat.py
+++ early-algorithms/combat.py
@@ -16,3 +16,19 @@ def combat(Player_Team, Enemy_Team) -> bool:
     # i cba to write this it's 2:30am 
     
     return combat_is_live
+
+def party_info(party, id = -1):
+    """Prints out info about everyone in a party.
+    
+    id (int, optional) : Opens a deeper menu with the list of ... for character
+        corresponding to id
+    """
+    i = 0 # Index in party list that will be iterated over
+    for character in party:
+        print(f"{i}. {character.name} | {character.hp}HP | {character.role}")
+        i += 1
+    # Enter -1 to exit or a number for more info on that character
+    menu_option = int(input(f"Enter a number to learn more about a character or"
+                            f" -1 to exit."))
+    while menu_option != -1:
+        print(party[menu_option])
