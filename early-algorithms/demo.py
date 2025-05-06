1232bf29cc3f86f15ac7fdae4a792da6df46c0c7
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-05-06 10:01:34 -0400

    Begin working on demo

---
+++ early-algorithms/demo.py
@@ -0,0 +1,10 @@
+import character
+# Start off by printing menu
+print(f"This is an irrelevant piece of text meant to represent the starting"
+      f" menu. It should be changed before demonstration.")
+print("Welcome!")
+# Print character options
+# Get team selection
+# Generate an enemy team
+# Battle???
+# Display shop if character won
