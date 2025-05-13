ac6e1679d77512ba8c5c61153c4247b9c5bedd5f
Ricardo H <101194496+ricardo-hz@users.noreply.github.com>
2025-05-13 19:02:48 -0400

    Finish shop

--- equipment.py
+++ equipment.py
@@ -11,59 +11,65 @@ ABILITIES = {
 }
 
 class Weapon:
-     """Representation of a Weapon.
-     
-     Attributes:
-         name (str): name of weapon
-         damage (int): damage of weapon
-         ability_included (None/list): ability given by weapon
-         hitMod (int): amount of additional hits this weapon adds to abilities.
-         by default, 0
-     """
-     def __init__(self, name: str, damage: int, ability_included: Ability = None, \
-         hitMod: int = 0):
-         # name of weapon, str
-         if isinstance(name, str):
-             self.name: str = name
-         else:
-             raise TypeError(f"Not valid type for name: {type(name)}")
-         
-         # weapon damage. an integer value
-         if isinstance(damage, int):
-             self.damage: int = damage
-         else:
-             raise TypeError(f"Not valid type for dmg: {type(damage)}")
-         
-         
-         # ability that is modified by weapon, has orig abil and new abil
-         self.ability_included: Ability = ability_included
-         # abilMod is list of two strings containing name of orig ability, name
-         # of enhanced ability with weapon
-         
-         self.hitMod: int = hitMod
+    """Representation of a Weapon.
+    
+    Attributes:
+        name (str): name of weapon
+        damage (int): damage of weapon
+        ability_included (None/list): ability given by weapon
+        hitMod (int): amount of additional hits this weapon adds to abilities.
+        by default, 0
+    """
+    def __init__(self, name: str, damage: int, ability_included: Ability = None, \
+        hitMod: int = 0):
+        # name of weapon, str
+        if isinstance(name, str):
+            self.name: str = name
+        else:
+            raise TypeError(f"Not valid type for name: {type(name)}")
+        
+        # weapon damage. an integer value
+        if isinstance(damage, int):
+            self.damage: int = damage
+        else:
+            raise TypeError(f"Not valid type for dmg: {type(damage)}")
+        
+        
+        # ability that is modified by weapon, has orig abil and new abil
+        self.ability_included: Ability = ability_included
+        # abilMod is list of two strings containing name of orig ability, name
+        # of enhanced ability with weapon
+        
+        self.hitMod: int = hitMod
+        
+    def __str__(self):
+        return(f"name: {self.name} | dmg: {self.damage}")
         
 class Armor:
-     """Representation of armor.
-     
-     Attributes:
-         name (str): name of weapon
-         damage (int): damage of weapon
-         ability_included (None/list): ability given by armor
-     """
-     def __init__(self, name: str, defense:int, \
-         ability_included: Ability = None):
-         # weapon damage. an integer value
-         if isinstance(defense, int):
-             self.defense: int = defense
-         else:
-             raise TypeError(f"Not valid type for defn: {type(defense)}")
+    """Representation of armor.
+    
+    Attributes:
+        name (str): name of weapon
+        damage (int): damage of weapon
+        ability_included (None/list): ability given by armor
+    """
+    def __init__(self, name: str, defense:int, \
+        ability_included: Ability = None):
+        # weapon damage. an integer value
+        if isinstance(defense, int):
+            self.defense: int = defense
+        else:
+            raise TypeError(f"Not valid type for defn: {type(defense)}")
+        
+        # name of weapon, str
+        if isinstance(name, str):
+            self.name: str = name
+        else:
+            raise TypeError(f"Not valid type for name: {type(name)}")
+        
+        # ability that is modified by weapon, has orig abil and new abil
+        self.ability_included: Ability = ability_included
          
-         # name of weapon, str
-         if isinstance(name, str):
-             self.name: str = name
-         else:
-             raise TypeError(f"Not valid type for name: {type(name)}")
-         
-         # ability that is modified by weapon, has orig abil and new abil
-         self.ability_included: Ability = ability_included
+    def __str__(self):
+        return(f"name: {self.name} | def: {self.defense}")
          
--- shop.py
+++ shop.py
@@ -1,45 +1,93 @@
 from equipment import *
 import character_class as cc
-from all_characters import ac
+import all_characters as ac
+import all_equipment as ae
 from gameplay import assemble_team
+import copy
 
-def shop(team):
-    # Team should be of type Player_Party from character_class
-    print("Welcome to the shop!")
-    print(f"The shop allows you to upgrade your team's existing armor and"
-          f" weapons.")
-    print(f"Note for each visit to the shop, you may only acquire one item "
-            f"for each character.")
-    for character in team:
-        print(f"Current character: {character.name}")
-        print("WEAPONS")
-        print("ARMOR")
+def get_next_weapon(character):
+    index = 0
+    for v in ae.WEAPONS[character.name]:
+        if character.weapon.name == v.name:
+            return(ae.WEAPONS[character.name][index + 1])
+        index += 1
+# Source: https://stackoverflow.com/questions/69642889/how-to-use-multiple-cases-in-match-switch-in-other-languages-cases-in-python-3
+def get_next_armor(character):
+    index = 0
+    match(character.name):
+        case("Pisces The Mage"):
+            armor_designation = "Cloth"
+        case("Aquarius the Priest" | "Sagitarrius the Archer" | "Gemini the Thief"):
+            armor_designation = "Leather"
+        case("Leo the Knight" | "Libra the Paladin"):
+            armor_designation = "Metal"
+        case _:
+            raise NotImplementedError # TODO: Protagonist has no armor
         
-        shop_choice = ''
-        while shop_choice not in ["WEAPONS", "ARMOR", "ABILITIES", "NONE"]:
-            shop_choice = input(f"Please select which shop you would like to "
-                                f"view. "
-                                f"Enter NONE to skip the shop for this "
-                                f"character. ")
-            match(shop_choice):
-                case("WEAPONS"):
-                    for weapon in WEAPONS:
-                        print(weapon)
-                    character.swap_weapon(Weapon(input().strip()))
-                    
-                case("ARMOR"):
-                    break
-                
-                case("ABILITIES"):
-                    print()
-                    print("Choose from one of the following abilities:")
-                    for ability in ABILITIES:
-                        print(ability)
-                    character.add_ability(Ability(input().strip()))
-                case("NONE"):
-                    break
-                case(default):
-                    print(f"{default} is not a valid entry.")
+    for v in ae.ARMOR[armor_designation]:
+        if character.armor.name == v.name:
+            return(ae.ARMOR[armor_designation][index + 1])
+        index += 1
 
+def shop(character):
+    """Gives a player the opportunity to upgrade weapons and armor.
+    """
+    print(f"Welcome to the shop, {character.name}!")
+    #print(f"The shop allows you to upgrade your team's existing armor and"
+          #f" weapons.")
+    #print(f"Note for each visit to the shop, you may only acquire one item "
+            #f"for each character.")
+    #for character in team:
+    print(f"Current Weapon: {character.weapon} | Next Weapon: {get_next_weapon(character)}")
+    print(f"Current Armor: {character.armor} | Next Armor {get_next_armor(character)}")
+    
+    shop_choice = ""
+    while shop_choice not in ["WEAPONS", "ARMOR", "NONE"]:
+        print(f"To upgrade to the next available weapon, type 'WEAPON' .")
+        print(f"To upgrade to the next available piece of armor, type 'ARMOR' .")
+        print(f"Exit the shop by typing 'NONE' .")
+        shop_choice = input().strip()
+        if shop_choice not in ["WEAPONS", "ARMOR", "NONE"]:
+            print(f"{shop_choice} is not a valid entry.")
+    
+    match(shop_choice):
+        case("WEAPON"):
+            character.swap_weapon(get_next_weapon(character))
+        case("ARMOR"):
+            character.swap_armor(get_next_armor(character))
+        case("NONE"):
+            pass
+        
+        
+        
+        
+        
+
+"""
+# This code gets the next available weapon for knight
+k = copy.deepcopy(ac.knight)
+
+print(f"New weapon: {k.weapon.name}")
+print("-------")
+print(get_next_weapon(k))
+print("-------")
+print()
+print("-------")
+print(get_next_armor(k))
+print("-------")
+"""
+"""
+m = copy.deepcopy(ac.mage)
+
+i = 0
+for v in ae.ARMOR["Cloth"]:
+    if m.armor.name == v.name:
+        print(m.armor.name)
+        print(ae.ARMOR["Cloth"][i + 1].name)
+    i += 1
+# This code gets the next available armor for mage
+        
+        
 team = assemble_team(ac.ALL_CHARACTERS)
 shop(team)
+"""
